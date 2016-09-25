ee1eb9a3ee353cd522120a496a5fcf4db58a0f53
Jiaxing <tian.jiaixng@gmail.com>
2016-09-25 19:17:17 -0400

    Update and rename Bash.sh to Bash_Regex.sh

---
+++ Bash_Regex.sh
@@ -0,0 +1,16 @@
+# Match letter and digits abc / 124
+# ANg Digit \d, any Non-Digit character \D
+# . any character; \. period
+# [abc] Match only a,b,c [^abc] Match not a,b,c
+# [a-z] character a to z; [1-9] numbers 1-9
+# \w any alphanumeric character \W any Non-alphanumeric character
+# {m} m Repetition; {m, n} m to n Repetition
+# * zero or more repetition; + one or more repetition; ? optional character
+# \s any whitespace; \S any non-white Space character
+# ^......$ indicate start to end of a line
+# (...) capture group; (.*) Capture all; (a(bc)) group capture; (abc|bdc) Match abc or bdc
+
+
+
+grep -P "^(\d{3}-|\([0-9]{3}\) )\d{3}-\d{4}$" file.txt
+
