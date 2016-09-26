a609be65344e75e46a7895ecbf209030bf7d742f
Jiaxing <tian.jiaixng@gmail.com>
2016-09-25 21:04:45 -0400

    Update Bash_Regex.sh

--- Bash_Regex.sh
+++ Bash_Regex.sh
@@ -14,13 +14,22 @@
 
 grep -P "^(\d{3}-|\([0-9]{3}\) )\d{3}-\d{4}$" file.txt
 
+
 # Read from the file file.txt and output the tenth line to stdout.
-count=0
-while read line && [ $count -le 10 ]; do
-    let 'count += 1'
-    if [ $count -eq 10 ]; then
-        echo $line
-        exit 0
-        #return
-    fi
+# Solution 1
+cnt=0
+while read line && [ $cnt -le 10 ]; do
+  let 'cnt = cnt + 1'
+  if [ $cnt -eq 10 ]; then
+    echo $line
+    exit 0
+  fi
 done < file.txt
+
+# Solution 2
+awk 'FNR == 10 {print }'  file.txt
+# OR
+awk 'NR == 10' file.txt
+
+# Solution 3
+sed -n 10p file.txt
