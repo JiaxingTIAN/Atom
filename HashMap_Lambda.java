f3e60282e73a4f693f782b388ae24e059e4bac3c
Jiaxing <tian.jiaixng@gmail.com>
2016-10-03 00:41:11 -0400

    Update HashMap_Lambda.java

--- HashMap_Lambda.java
+++ HashMap_Lambda.java
@@ -61,6 +61,24 @@ public class Solution{
         heap.offer(6);
         heap.offer(3);
         heap.forEach(System.out::print);    //643
+        
+        // String to be scanned to find the pattern.
+      String line = "This order was placed for QT3000! OK?";
+      String pattern = "(.*)(\\d+)(.*)";
+
+      // Create a Pattern object
+      Pattern r = Pattern.compile(pattern);
+
+      // Now create matcher object.
+      Matcher m = r.matcher(line);
+      if (m.find( )) {
+         System.out.println("Found value: " + m.group(0) );
+         System.out.println("Found value: " + m.group(1) );
+         System.out.println("Found value: " + m.group(2) );
+      }else {
+         System.out.println("NO MATCH");
+      }
+
     }
     
     class animate implements Runnable{
